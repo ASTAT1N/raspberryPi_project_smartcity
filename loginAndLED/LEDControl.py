1a84c16c20881c15f954b1512d246451401502cf
Huisu Lim <4922865@gmail.com>
2019-08-15 22:27:47 +0900

    add gpio control commend

--- loginAndLED/LEDControl.py
+++ loginAndLED/LEDControl.py
@@ -1,1 +1,16 @@
-print("hello")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(6, GPIO.OUT)
+
+while True:
+    #켜기
+    GPIO.output(6, GPIO.HIGH)
+    
+    #시간 제어
+    time.sleep(1)
+
+    #끄기
+    GPIO.output(6, GPIO.LOW)
